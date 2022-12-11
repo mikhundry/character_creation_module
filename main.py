156a10c4dcf04afdbe6e40c466a9f3a393407923
Михаил Дрожжин <drmisha79@gmail.com>
2022-12-11 13:10:18 +0700

    '

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from random import randint
 
 
+from graphic_arts.start_game_banner import run_screensaver
+
+
 def attack(char_name: str, char_class: str) -> str:
+    """Рассчитывает атаку."""
     if char_class == 'warrior':
         return (
             f'{char_name} нанёс урон противнику равный {5 + randint(3, 5)}')
@@ -15,6 +19,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Рассчитывает защиту."""
     if char_class == 'warrior':
         return (
             f'{char_name} блокировал {10 + randint(5, 10)} урона')
@@ -28,6 +33,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Рассчитывает специальное умение."""
     if char_class == 'warrior':
         return (
             f'{char_name} применил специальное умение'
@@ -42,6 +48,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Отвечает за начало тренировки."""
     if char_class == 'warrior':
         print(
             f'{char_name}, ты Воитель — отличный боец ближнего боя.')
@@ -69,6 +76,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class():
+    """Выбор класса."""
     approve_choice = None
     char_class = None
     while approve_choice != 'y':
@@ -95,7 +103,8 @@ def choice_char_class():
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -105,6 +114,3 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
